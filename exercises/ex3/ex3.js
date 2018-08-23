4463e15583c5bc63f810c9c7da407a2e99db1216
chroda <chroda@chroda.com.br>
2018-08-23 14:39:13 -0300

    ex3

--- exercises/ex3/ex3.js
+++ exercises/ex3/ex3.js
@@ -1,10 +1,20 @@
-function upper(strings,...values) {}
+function upper(strings,...values) {
+	var s = '';
+	for (let i = 0; i < strings.length; i++) {
+		if(i > 0){
+			s += values[i-1].toUpperCase();
+		};
+		s += strings[i];
+	}
+	return s;
+}
 
 var name = "kyle",
 	twitter = "getify",
 	classname = "es6 workshop";
 
 console.log(
-	`Hello ____ (@____), welcome to the ____!` ===
+	upper
+	`Hello ${name} (@${twitter}), welcome to the ${classname}!` ===
 	"Hello KYLE (@GETIFY), welcome to the ES6 WORKSHOP!"
 );
