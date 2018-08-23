57c52ba9187850e29d83a877f7cc324e43a15e48
chroda <chroda@chroda.com.br>
2018-08-23 16:03:57 -0300

    ex4

--- exercises/ex4/ex4.js
+++ exercises/ex4/ex4.js
@@ -1,14 +1,23 @@
 var numbers = {
-	// ..
+	*[Symbol.iterator]({
+		start = 0,
+		end = 100,
+		step = 1,
+	} = {}) {
+		for (let num = start; num <= end; num += step) {
+			yield num;
+		}
+	} 
 };
 
 // should print 0..100 by 1s
 for (let num of numbers) {
-	console.log(num);
+	// console.log(num);
 }
 
-// should print 6..30 by 4s
-console.log("My lucky numbers are: ____");
+
+// should print 6..30 by 4step
+console.log(`My lucky numbers are: ${[...numbers[Symbol.iterator]({start:6,end:30,step:4})]}`);
 
 // Hint:
 //     [...numbers[Symbol.iterator]( ?? )]
